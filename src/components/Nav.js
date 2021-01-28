57228290bcfdfec2d4b8ba68ea183cdecedfc31e
Adam <adam.kowalczuk@poczta.fm>
2021-01-28 19:48:07 +0100

    dodano górną nawigację

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -18,10 +18,7 @@ export default function Nav() {
                 <div className="nav-option"><i className="fas fa-hamburger"></i></div>
                 <div className="nav-option"><i className="fas fa-grin-tears"></i></div>
                 <div className="nav-option"><i className="fas fa-pizza-slice"></i></div>
-                <div className="nav-option"><i className="fas fa-dice"></i></div>
-                <div className="nav-option"><i className="fas fa-dog"></i></div>
-                <div className="nav-option"><i className="fas fa-dumbbell"></i></div>
-                <div className="nav-option"><i className="fas fa-grin-hearts"></i></div>
+
             </div>
         </>
     )
