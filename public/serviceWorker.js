958a57f612ad45b4bd8e47190be6180a6f8cf4e0
Adam <adam.kowalczuk@poczta.fm>
2021-03-09 22:16:37 +0100

    zmiana start_url

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -93,7 +93,11 @@ self.addEventListener("fetch", (evt) => {
           })
         );
       })
-      .catch(() => caches.match("/fallback.html"))
+      .catch(() => {
+        if (evt.request.url.indexOf(".html") > -1) {
+          return caches.match("/fallback.html");
+        }
+      })
   );
 });
 
